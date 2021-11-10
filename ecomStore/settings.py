9c1f03dadeb6c4a1521fa39b3c0c0852ac187d9c
Umesh Kumar <uk481281@gmail.com>
2021-11-10 17:50:12 +0530

    firstHerokuCommit

--- ecomStore/settings.py
+++ ecomStore/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-e@!*iz-m#e-ak(#fmmfpbn@^7fd&13c&tc1q($r#6@8hfen!8y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -130,3 +132,5 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/var/www/static/',
 ]
+
+django_heroku.settings(locals())
